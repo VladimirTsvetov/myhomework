3f54ae1c4d9de65e2276840824840196ad104ce5
VladimirTsvetov <96310995+VladimirTsvetov@users.noreply.github.com>
2022-09-26 11:38:57 +0300

    Revert "загружено домашнее задание 1"

--- geek-cloud/geek-cloud-client/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.geekbrains.sep22.geekcloudclient {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.geekbrains.sep22.geekcloudclient to javafx.fxml;
-    exports com.geekbrains.sep22.geekcloudclient;
-}
